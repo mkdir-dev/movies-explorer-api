14b58271a8227675fa775085f64bdbe295cebe8b
Mikhail Koryukov <mezhovkin@yandex.ru>
2021-08-20 11:16:58 +0500

    feat: create route POST /signup

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const { createUser } = require('./controllers/users');
+
 const usersRoutes = require('./routes/users');
 const moviesRoutes = require('./routes/movies');
 
@@ -14,6 +16,11 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
   useCreateIndex: true,
   useFindAndModify: false,
 });
+
+// создать пользователя
+app.post('/signup', createUser);
+
+// POST /signin
 
 app.use('/users', usersRoutes);
 app.use('/movies', moviesRoutes);
--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,12 @@
+const bcrypt = require('bcryptjs');
+// const jwt = require('jsonwebtoken');
+
 const User = require('../models/user');
 
 const NotFoundError = require('../errors/404 - NotFoundError');
 const BadRequestError = require('../errors/400 - BadRequestError');
 const InternalServerError = require('../errors/500 - InternalServerError');
+const ConflictError = require('../errors/409 - ConflictError');
 
 const {
   SUCCESS_OK,
@@ -47,3 +51,36 @@ module.exports.updateUser = (req, res, next) => {
     })
     .catch(next);
 };
+
+// создать пользователя с переданными в теле
+// email, password и name
+// POST /signup
+module.exports.createUser = (req, res, next) => {
+  const { email, password, name } = req.body;
+
+  bcrypt.hash(password, 8)
+    .then((hash) => User.create({
+      email,
+      password: hash,
+      name,
+    })
+      .then(() => res.status(SUCCESS_OK).send({
+        email, name,
+      }))
+      .catch((err) => {
+        if (err.name === 'ValidationError') {
+          throw new BadRequestError('Ошибка валидации при создании пользователя');
+        } else if (err.name === 'CastError') {
+          throw new BadRequestError('Переданы некорректные данные при создании пользователя');
+        }
+        if (err.name === 'MongoError' && err.code === 11000) {
+          throw new ConflictError('Пользователь с таким Email уже зарегистрирован');
+        }
+        throw new InternalServerError('Ошибка сервера. Ошибка по-умолчанию');
+      }))
+    .catch(next);
+};
+
+// проверяет переданные в теле почту и пароль
+// и возвращает JWT
+// POST /signin
