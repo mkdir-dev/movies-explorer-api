f95d4315036cd38d262c96f6cc115347da68d60b
Mikhail Koryukov <mezhovkin@yandex.ru>
2021-08-21 16:56:02 +0500

    feat: add err 404

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,16 @@ const auth = require('../middlewares/auth');
 const usersRoutes = require('./users');
 const moviesRoutes = require('./movies');
 
+const NotFoundError = require('../errors/404 - NotFoundError');
+
 router.post('/signin', login);
 router.post('/signup', createUser);
 
 router.use('/users', auth, usersRoutes);
 router.use('/movies', auth, moviesRoutes);
+
+router.get('*', () => {
+  throw new NotFoundError('Запрашиваемый ресурс не найден');
+});
 
 module.exports = router;
